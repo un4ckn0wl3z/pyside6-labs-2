4546f1e5fa9e1a6be5ab82ab7b64d25c232e671b
un4kn0wl3z <admin@unknownclub.net>
2023-03-31 16:58:22 +0700

    first commit

---
+++ 002.signals_and_slots/001_pushbutton_signals_and_slots.py
@@ -0,0 +1,25 @@
+from PySide6.QtWidgets import (
+  QApplication,
+  QMainWindow,
+  QPushButton,
+)
+
+import sys
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("My App")
+        button = QPushButton("Press Me!")
+        button.setCheckable(True)
+        button.clicked.connect(self.the_button_was_clicked)
+        # Set the central widget of the Window.
+        self.setCentralWidget(button)
+    
+    def the_button_was_clicked(self):
+        print("Clicked!")
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec()
---
+++ 002.signals_and_slots/003_changing_interface.py
@@ -0,0 +1,22 @@
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
+import sys
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("My App")
+        self.button = QPushButton("Press Me!")
+        self.button.clicked.connect(self.the_button_was_clicked)
+        # Set the central widget of the Window.
+        self.setCentralWidget(self.button)
+
+    def the_button_was_clicked(self):
+        self.button.setText("You already clicked me.")
+        self.button.setEnabled(False)
+        # Also change the window title.
+        self.setWindowTitle("My Oneshot App")
+
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec()
